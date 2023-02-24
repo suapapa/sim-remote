7b6a8851244a7e57d73070448540e6e3c9417cc3
Homin Lee <homin.lee@suapapa.net>
2023-02-24 12:14:20 +0900

    switching src works

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import tv
 from flask import Flask, current_app
-from handler import tv_bp, tv_key_bp, tv_audio_bp
+from handler import tv_bp, tv_key_bp, tv_src_bp, tv_audio_bp
 
 app = Flask(__name__)
 
@@ -9,7 +9,7 @@ with app.app_context():
 
 app.register_blueprint(tv_bp)
 app.register_blueprint(tv_key_bp)
-# app.register_blueprint(tv_src_bp)
+app.register_blueprint(tv_src_bp)
 app.register_blueprint(tv_audio_bp)
 
 if __name__ == '__main__':
--- handler.py
+++ handler.py
@@ -5,7 +5,7 @@ import tv
 
 tv_bp = Blueprint('tv', __name__)
 tv_key_bp = Blueprint('key', __name__)
-# tv_src_bp = Blueprint('src', __name__)
+tv_src_bp = Blueprint('src', __name__)
 tv_audio_bp = Blueprint('audio', __name__)
 
 
@@ -51,6 +51,45 @@ def put_key(key):
     exec(f'input_ctl.{key}()')
 
     response = make_response(jsonify({'msg': f'{key} sent'}))
+    response.status_code = 200
+    return response
+
+
+@ tv_src_bp.route('/src', methods=['GET'])
+def get_src():
+    tv_client = current_app.tv_client
+    srcs = tv_cntl.SourceControl(tv_client).list_sources()
+    src_labels = list(map(lambda x: x['label'], srcs))
+
+    response = make_response(jsonify({'data': src_labels}))
+    response.status_code = 200
+    return response
+
+
+@ tv_src_bp.route('/src/<src>', methods=['PUT'])
+def put_src(src):
+    print(f'src: {src}')
+
+    tv_client = current_app.tv_client
+
+    srcs = tv_cntl.SourceControl(tv_client).list_sources()
+    src_labels = list(map(lambda x: x['label'], srcs))
+    i = 0
+    for s in src_labels:
+        if src == s:
+            break
+        i += 1
+
+    if i == len(src_labels):
+        response = make_response(jsonify({'msg': f'Invalid source: {src}'}))
+        response.status_code = 400
+        return response
+
+    tv_cntl.SourceControl(tv_client).set_source(srcs[i])
+
+    notify_tv(f'Set source to {src}')
+
+    response = make_response(jsonify({'msg': f'set source to {src}'}))
     response.status_code = 200
     return response
 
