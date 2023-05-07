2b32baca3d27fd460cdf7c172111ff4735cab3a2
Homin Lee <homin.lee@suapapa.net>
2023-05-07 13:27:20 +0900

    restart when exception happen

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import webostv
+import sys
 from flask import Flask, current_app
 from handler import tv_bp, tv_key_bp, tv_src_bp, tv_audio_bp, tv_app_bp
 
@@ -13,6 +14,13 @@ app.register_blueprint(tv_key_bp)
 app.register_blueprint(tv_src_bp)
 app.register_blueprint(tv_audio_bp)
 app.register_blueprint(tv_app_bp)
+
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    print(f"Caught an exception: {e}")
+    sys.exit(-1)
+
 
 if __name__ == '__main__':
     app.run()  # :5000
